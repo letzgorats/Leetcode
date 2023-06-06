09a36471b1bd37f31fbbfaac63bddcb1cdc0fa21
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2023-06-06 21:08:41 +0900

    binarysearch, deque, levelOrder, bfs, queue

---
+++ leetcode/102/BinaryTreeLevelOrderTraversal.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        
+        print(root)
+        if not root:
+            return None
+        
+        queue = deque([root])
+        answer = []
+
+        while queue:
+            level = []
+            length = len(queue)
+            for _ in range(length):
+                now = queue.popleft()
+                level.append(now.val)
+                if now.left:
+                    queue.append(now.left)
+                if now.right:
+                    queue.append(now.right)
+            answer.append(level)
+        
+        return answer
