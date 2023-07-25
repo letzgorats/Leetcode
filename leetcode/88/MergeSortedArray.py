28d6922095f2afa85993f67a5136eeb031340dc1
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2023-07-24 21:13:52 -0700

    extends / merge sorted array

--- leetcode/88/MergeSortedArray.py
+++ leetcode/88/MergeSortedArray.py
@@ -1,3 +1,5 @@
+# extends
+
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
 
@@ -5,6 +7,17 @@ class Solution:
         nums1.extend(nums2[:n])
         nums1.sort()
 
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
+
+# merge_sort
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        
+        while m > 0 and n > 0:
+            if nums1[m-1] >= nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
+                else:
+                    nums1[m+n-1] = nums2[n-1]
+                    n -= 1
+            if n > 0:
+                nums1[:n] = nums2[:n]
