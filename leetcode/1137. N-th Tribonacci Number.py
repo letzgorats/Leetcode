9569b757ed1515117cf5cf595028617da0c3cc16
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2023-08-21 23:42:20 -0700

    prefix sum, modular, dp

---
+++ leetcode/1137. N-th Tribonacci Number.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    
+    def tribonacci(self, n):
+
+        triresult = [0,1,1]
+
+        for i in range(3,n+1):
+            triresult[i%3] = sum(triresult)
+        
+        return triresult[n%3]
+
+
+
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+
+        dp = [0,1,1]
+
+        for i in range(3,n+1):
+
+            dp.append(dp[i-3] + dp[i-2]+ dp[i-1])
+
+        return dp[n] 
+
+
+
+
+
+
