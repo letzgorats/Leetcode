6fa9339055ef156d9bff7e9d08c6dd616929f7cc
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2023-08-22 01:55:37 -0700

    topological sort, graph, deque, prerequisites, indegree

--- leetcode/210/Course Schedule II.py
+++ leetcode/210/Course Schedule II.py
@@ -38,3 +38,7 @@ class Solution(object):
 
         else:   
             return result
+
+
+
+# topological sort -> (https://letzgorats.tistory.com/entry/%EA%B7%B8%EB%9E%98%ED%94%84-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-2-%EC%BD%94%EB%94%A9-%ED%85%8C%EC%8A%A4%ED%8A%B8%EC%97%90%EC%84%9C-%EC%9E%90%EC%A3%BC-%EB%93%B1%EC%9E%A5%ED%95%98%EB%8A%94-%EA%B8%B0%ED%83%80-%EA%B7%B8%EB%9E%98%ED%94%84-%EC%9D%B4%EB%A1%A0)
