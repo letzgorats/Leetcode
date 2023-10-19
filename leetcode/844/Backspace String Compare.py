912d15c0071db0af965334a7c7bb354302a3e3ec
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2023-10-19 23:57:26 +0900

    if, stack, pop

---
+++ leetcode/844/Backspace String Compare.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def backspaceCompare(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+
+        tmp1 = []
+        for alpha in s:
+            if alpha == "#" and len(tmp1)!=0:
+                tmp1.pop()
+            elif alpha != '#':
+                tmp1.append(alpha)
+        
+        tmp2 = []
+        for alpha in t:
+            if alpha == "#" and len(tmp2)!=0:
+                tmp2.pop()
+            elif alpha != "#":
+                tmp2.append(alpha)
+
+        print(tmp1)
+        print(tmp2)
+        if tmp1 == tmp2 :
+            return True
+        else:
+            return False
