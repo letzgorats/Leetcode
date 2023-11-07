35087db9d36d2a4e7141ded8183b366c66e81fd9
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2023-11-08 07:59:18 +0900

    bin(), unsigned integer, bit mask , &

---
+++ leetcode/191/Number of 1 Bits.py
@@ -0,0 +1,29 @@
+# my sol 
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+        return bin(n).count('1')
+
+
+# sol 2
+
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+        answer = 0
+        mask = 1
+
+        for i in range(32):
+            if (n & mask) != 0: # check 1
+                answer += 1
+            mask <<= 1
+        
+        return answer
