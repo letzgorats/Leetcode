cf57927d9d893090a9c3c7763c9ea33719d10ed1
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2023-12-03 23:49:07 +0900

    chebyshev distance

---
+++ leetcode/1266/Minimum Time Visiting All Points.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def minTimeToVisitAllPoints(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        answer = 0
+        for i in range(len(points)-1):
+
+            x1, y1 = points[i][0], points[i][1] 
+            x2, y2 = points[i+1][0], points[i+1][1] 
+
+            # if x1-y1 == x2-y2:
+            #     answer += abs(x1-x2)
+            # else:
+            #     
+            answer += max(abs(x1-x2),abs(y1-y2))
+                # answer += int(sqrt(abs(x1-x2) ** 2 + abs(y1-y2) ** 2))
+                
+        return answer
+            
+            
+        
