b50e6c8e28ad1f160f6540c779473a9b2b16b3de
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2023-12-18 10:14:07 +0900

    sort

---
+++ leetcode/1913/Maximum Product Difference Between Two Pairs.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def maxProductDifference(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        nums.sort()
+
+        return (nums[-2] * nums[-1]) - (nums[1]*nums[0])
