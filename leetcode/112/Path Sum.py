1a6165275013bab3d92bfa941d4f17feb3b9c04b
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2023-12-18 21:56:09 +0900

    tree, root, recursive, dfs, binary tree, preorder, cumulation
    wrong

--- leetcode/112/Path Sum.py
+++ leetcode/112/Path Sum.py
@@ -10,3 +10,39 @@ class Solution(object):
             return targetSum == 0
         
         return self.hasPathSum(root.left,targetSum) or self.hasPathSum(root.right,targetSum)
+
+
+# second-try 
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    
+    
+    def hasPathSum(self, root, targetSum):
+        """
+        :type root: TreeNode
+        :type targetSum: int
+        :rtype: bool
+        """
+
+        def dfs(root,cost):
+        
+            if not root:
+                return False
+
+            cost += root.val
+
+            
+            # only leaf node
+            if (not root.left and not root.right):
+                return cost == targetSum
+
+            return dfs(root.left,cost) or dfs(root.right,cost)
+
+
+        return dfs(root,0)
