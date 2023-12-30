7d48c7648339a841ccc8f67c67ce41804feac3e9
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2023-12-30 11:16:47 +0900

    hash, dict, count, modular

---
+++ leetcode/1897/Redistribute Characters to Make All Strings Equal.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def makeEqual(self, words):
+        """
+        :type words: List[str]
+        :rtype: bool
+        """
+        if len(words)==1:
+            return True
+
+        count = defaultdict(int)
+
+        for word in words:
+            for c in word:
+                count[c] += 1
+
+        for k,v in count.items():
+            if v == 1:
+                return False
+            else:
+                if v % len(words) != 0:
+                    return False
+        
+        return True
