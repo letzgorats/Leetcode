418daf4520fe765b2b154ea5195f0c5974f2f19c
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2023-12-30 11:20:49 +0900

    Update Redistribute Characters to Make All Strings Equal.py

--- leetcode/1897/Redistribute Characters to Make All Strings Equal.py
+++ leetcode/1897/Redistribute Characters to Make All Strings Equal.py
@@ -21,3 +21,21 @@ class Solution(object):
                     return False
         
         return True
+
+
+class Solution(object):
+    def makeEqual(self, words):
+        """
+        :type words: List[str]
+        :rtype: bool
+        """
+
+        n = len(words)
+
+        ans = ''.join(words)
+        res = set(ans)
+
+        for i in res:
+            if ans.count(i) % n != 0:
+                return False
+        return True
