36c8bcdfe16faeceaf82323c5ead0a1e805f069f
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2024-01-18 13:25:04 +0900

    dp, pattern, cache, accumulative

--- leetcode/70/Climbing Stairs.py
+++ leetcode/70/Climbing Stairs.py
@@ -17,3 +17,24 @@ class Solution(object):
         # 1+1+2
         # 2+1+1
         # 2+2
+
+
+# second sol
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        # 1 
+        # 2 -> 2 , 1 + 1
+        # 3 -> 1 + 2 , 1 + 1 + 1, 2 + 1
+        # 4 -> 2 + 2, 1 + 1 + 2
+        #   -> 2 + 1 + 1, 1 + 1 + 1 + 1, 1 + 2 + 1
+        # dp[2] + dp[3]
+
+        dp = (n+1) * [0]
+        dp[0] = 1
+        dp[1] = 1 
+        for i in range(2,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
+        
