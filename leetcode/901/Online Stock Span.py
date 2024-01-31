137d0e60092366f32ca6eab9b8d585499e31e03f
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2024-01-31 14:56:55 +0900

    stock, stack, monotonic, price

---
+++ leetcode/901/Online Stock Span.py
@@ -0,0 +1,25 @@
+class StockSpanner(object):
+
+    def __init__(self):
+
+        self.prices = []
+
+    def next(self, price):
+        """
+        :type price: int
+        :rtype: int
+        """
+        stack = self.prices
+        span = 1
+
+        while stack and stack[-1][0] <= price:
+
+            span += stack[-1][1]
+            stack.pop()
+        
+        stack.append((price,span))
+  
+        return span
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
