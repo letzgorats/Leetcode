21b8f2e81c56da59be12e083ab26a1b2369342b4
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2024-04-01 15:23:34 +0900

    strip, string, len, split(), len

--- leetcode/58/Length of Last Word.py
+++ leetcode/58/Length of Last Word.py
@@ -1,3 +1,4 @@
+# first try 
 class Solution(object):
     def lengthOfLastWord(self, s):
         """
@@ -7,3 +8,15 @@ class Solution(object):
         
         last = s.strip(" ").split(" ")[-1]
         return len(last)
+
+
+# second try solution
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+
+        return len(list(s.split())[-1])
