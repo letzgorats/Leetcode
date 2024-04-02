374dabb1ec768ff4d4466e6a8a023899c832f81d
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2024-04-02 11:58:48 +0900

    index(), zip(), Isomorphic, hash, string, defaultdict, count, len

--- leetcode/205/IsomorphicStrings.py
+++ leetcode/205/IsomorphicStrings.py
@@ -1,4 +1,5 @@
-lass Solution:
+# first try
+class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         
         answer = False
@@ -19,3 +20,40 @@ lass Solution:
                 answer = True
             
             return answer
+
+
+# second solution
+from collections import defaultdict
+class Solution(object):
+    def isIsomorphic(self, s, t):
+
+        counts = defaultdict(list)
+        countt = defaultdict(list)
+
+        for idx,st in enumerate(s):
+
+            counts[st].append(idx)
+        
+        for idx,tt in enumerate(t):
+
+            countt[tt].append(idx)
+
+        for k,v in counts.items():
+
+            if v not in countt.values():
+                return False
+        
+        return True
+
+
+# others solution
+class Solution(object):
+    def isIsomorphic(self, s, t):
+
+        return len(set(zip(s,t))) == len(set(s)) == len(set(t))
+
+
+            
+
+        
+        
