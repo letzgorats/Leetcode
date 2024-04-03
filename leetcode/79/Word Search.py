e7be3385f197d5796f2fe7bfd99f407bd3aa3d34
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2024-04-03 11:38:15 +0900

    word search, dfs, backtracking, or, index, bfs

--- leetcode/79/Word Search.py
+++ leetcode/79/Word Search.py
@@ -32,54 +32,35 @@ class Solution(object):
         return False
     
 
-# wrong solution - bfs
+# bfs solution
 from collections import deque
 
-class Solution(object):
+class Solution:
     def exist(self, board, word):
-
-        def bfs(r,c,deq):
-
-            queue = deque([(r,c)])
-            
-            dr = [-1,1,0,0]
-            dc = [0,0,-1,1]
-            visited = [[False] * m for _ in range(n)]
-            visited[r][c] = True
-
-            while queue:
-                if len(deq) == 0:
-                    return True
-
-                w = deq.popleft()
-                r, c = queue.popleft()
-                for d in range(4):
-                    nr, nc = r + dr[d], c + dc[d]
-                    
-                    if 0<= nr < n and 0 <= nc < m and not visited[nr][nc]:
-                        if board[nr][nc] == w:
-                            queue.append((nr,nc))
-                            visited[nr][nc] = True
-                        
-
+        if not board:
             return False
-
+        n, m = len(board), len(board[0])
         
-        start = word[0]
-        start_point = deque([])
-        n = len(board)
-        m = len(board[0])
+        # 주어진 위치와 단어의 인덱스에서 시작하여 word를 찾을 수 있는지 확인하는 BFS 함수
+        def bfs(r, c):
+            queue = deque([(r, c, 0, {(r, c)})])  # (현재 행, 현재 열, 단어의 인덱스, 방문한 위치 집합)
+            while queue:
+                r, c, index, visited = queue.popleft()
+                if index == len(word) - 1:
+                    return True
+                # 상하좌우 탐색
+                for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < n and 0 <= nc < m and (nr, nc) not in visited and board[nr][nc] == word[index + 1]:
+                        new_visited = visited | {(nr, nc)}  # 현재 위치 추가
+                        queue.append((nr, nc, index + 1, new_visited))
+            return False
         
         for i in range(n):
             for j in range(m):
-                if start == board[i][j]:
-                    start_point.append((i,j))
-        
-        while start_point:
-            
-            deq = deque(list(word[1:]))
-            i, j = start_point.popleft()
-            if bfs(i,j,deq):
-                return True
+                if board[i][j] == word[0]:  # 단어의 첫 글자와 일치하는 모든 칸에서 BFS 시작
+                    if bfs(i, j):
+                        return True
         
         return False
+
