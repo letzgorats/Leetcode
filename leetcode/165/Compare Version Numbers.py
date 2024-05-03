d89da594f60378e9a552dd2f218956fd274bf895
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2024-05-03 20:49:38 +0900

    string, split, extend, two pointers

---
+++ leetcode/165/Compare Version Numbers.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def compareVersion(self, version1, version2):
+
+        v1 = list(map(int,version1.split('.')))
+        v2 = list(map(int,version2.split('.')))
+
+        if len(v1) != len(v2):
+
+            if len(v1) < len(v2):
+                v1.extend([0] * (len(v2)-len(v1)))
+            elif len(v1) > len(v2):
+                v2.extend([0] * (len(v1)-len(v2)))
+
+        for i in range(len(v1)):
+
+            if v1[i] < v2[i]:
+                return -1
+            elif v1[i] > v2[i]:
+                return 1
+
+        return 0
