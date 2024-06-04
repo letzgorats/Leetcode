cd7fc335f1d37bffa624f0cc54d0ddd526577e7f
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2024-06-04 13:06:03 +0900

    palindrome, hash, dict, counter

--- leetcode/409/LongestPalindrome.py
+++ leetcode/409/LongestPalindrome.py
@@ -1,3 +1,4 @@
+# solution 1
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         
@@ -20,4 +21,34 @@ class Solution:
                 answer += ( value -1 )
         if odd_exist:
             answer += 1
+        return answer
+
+
+
+# second solution - 2024-06-04
+
+class Solution(object):
+    def longestPalindrome(self, s):
+
+        count = {}
+        for i in range(len(s)):
+            count[s[i]] = count.get(s[i],0)+1
+
+        if len(count) == 1:
+            for k,v in count.items():
+                return v
+
+        answer = 0
+        check = False
+        for k,v in count.items():
+
+            if v % 2 == 0:
+                answer += v
+            elif v % 2 == 1:
+                answer += (v-1)
+                check = True
+        
+        if check:
+            answer += 1
+
         return answer
