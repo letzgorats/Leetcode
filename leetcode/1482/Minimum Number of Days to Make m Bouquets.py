eaf5838dd9f4c43eb029fc98565e4e334bc90f04
letzgorats <a01030659322@gmail.com>
2024-06-19 14:18:51 +0900

    Binary Search, left, right, array,divied and conquer

---
+++ leetcode/1482/Minimum Number of Days to Make m Bouquets.py
@@ -0,0 +1,74 @@
+# solution - Binary Search (O(Nlog(Max(Bloomday))))
+class Solution(object):
+    def minDays(self, bloomDay, m, k):
+
+
+        if m * k > len(bloomDay):
+            return -1
+
+        def canMakeBouquets(days):
+
+            bouquets, flowers = 0, 0
+            for bloom in bloomDay:
+
+                if bloom <= days:
+                    flowers += 1
+                    if flowers == k:
+                        bouquets += 1
+                        flowers = 0
+
+                else:
+                    flowers = 0
+
+                if bouquets == m:
+                    return True
+
+            return False
+
+        left, right = min(bloomDay), max(bloomDay)
+
+        while left <= right:
+
+            mid = (left + right) // 2
+            if canMakeBouquets(mid):
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return left
+
+# TLE solution - brute force
+class Solution(object):
+    def minDays(self, bloomDay, m, k):
+        """
+        :type bloomDay: List[int]
+        :type m: int
+        :type k: int
+        :rtype: int
+        """
+
+        if m * k > len(bloomDay):
+            return -1
+
+        dayCandi = sorted(list(set(bloomDay)))
+        n = len(bloomDay)
+
+        for d in dayCandi:
+
+            bouquets = 0
+            flowers = 0
+
+            for i in range(n):
+
+                if bloomDay[i] <= d:
+                    flowers += 1
+                    if flowers == k:
+                        bouquets += 1
+                        flowers = 0
+                else:
+                    flowers = 0
+
+            if bouquets >= m:
+                return d
+
+        return -1
