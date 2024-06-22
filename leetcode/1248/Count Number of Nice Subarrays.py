a69830b7e73fdb213bd67623b4668f69e9f1d32e
letzgorats <a01030659322@gmail.com>
2024-06-22 12:00:17 +0900

    subArray,prefix_sum, sliding window, accumulative sum, hash table, array

---
+++ leetcode/1248/Count Number of Nice Subarrays.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def numberOfSubarrays(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+
+        for i in range(len(nums)):
+            if nums[i] % 2 == 0:
+                nums[i] = 0
+            else:
+                nums[i] = 1
+
+        count = {0:1}
+        current_sum = 0
+        answer = 0
+
+        for num in nums:
+
+            current_sum += num
+            if current_sum-k in count:
+                answer += count[current_sum-k]
+            count[current_sum] = count.get(current_sum,0) + 1
+
+        return answer
