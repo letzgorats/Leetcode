ecfb56807332fe838846d4f54dd3c0eb3c0af793
letzgorats <a01030659322@gmail.com>
2024-07-01 00:38:03 +0900

    Update README.md

--- leetcode/1579/Remove Max Number of Edges to Keep Graph Fully Traversable.py
+++ leetcode/1579/Remove Max Number of Edges to Keep Graph Fully Traversable.py
@@ -28,24 +28,31 @@ class Disjoint_Set_Union:
 class Solution(object):
     def maxNumEdgesToRemove(self, n, edges):
 
-        # DSU 자료구조 초기화
+        # DSU 자료구조 초기
         dsu_alice = Disjoint_Set_Union(n)
         dsu_bob = Disjoint_Set_Union(n)
         dsu_total = Disjoint_Set_Union(n)
 
+        # 간선을 타입에 따라 내림차순
+        # (타입 3 간선을 먼저 처리하도록 하는 역할)
+        # 타입 3 간선은 alice, bob 둘 다 사용할 수 있어
+        # 최대한 활용하는 것이 간선을 절약하는 데 유리하다.
+
         edges.sort(reverse=True, key=lambda x: x[0])
 
-        total_edges = 0
-        alice_edges = 0
-        bob_edges = 0
+        total_edges = 0 # 총 사용된 간선 수
+        alice_edges = 0  # alice 가 사용한 간선 수
+        bob_edges = 0 # bob 이 사용한 간선 수
 
+        # 간선을 하나씩 처리
         for edge in edges:
 
             t, a, b = edge
-            a -= 1
-            b -= 1
+            a -= 1  # 인덱스를 0 부터 시작하도록 조정
+            b -= 1  # 인덱스를 0 부터 시작하도록 조정
 
             if t == 3:
+                # 타입 3 간선: 전체, alice, bob 모두에 추가
                 if dsu_total.union(a, b):
                     total_edges += 1
                     if dsu_alice.union(a, b):
@@ -54,16 +61,21 @@ class Solution(object):
                         bob_edges += 1
 
             elif t == 1:
+                # 타입 1 간선: alice 에만 추가
                 if dsu_alice.union(a, b):
                     alice_edges += 1
                     total_edges += 1
 
             elif t == 2:
+                # 타입 2 간선: bob 에만 추가
                 if dsu_bob.union(a, b):
                     bob_edges += 1
                     total_edges += 1
 
-        if alice_edges == n - 1 and bob_edges == n - 1:
+        print(alice_edges)
+        print(bob_edges)
+        if (alice_edges == n - 1) and (bob_edges == n - 1):
+            # 제거할 수 있는 간선 수 반환
             return len(edges) - total_edges
         else:
             return -1
