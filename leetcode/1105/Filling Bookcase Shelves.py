cca76b78f54aa2e471d929362d116cb4b27b0d53
letzgorats <a01030659322@gmail.com>
2024-08-01 12:58:34 +0900

    dp,dynamic programming,width,height,min,max

--- leetcode/1105/Filling Bookcase Shelves.py
+++ leetcode/1105/Filling Bookcase Shelves.py
@@ -1,4 +1,19 @@
 class Solution:
     def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
 
+        n = len(books)
+        dp = [float('inf')] * (n + 1)
+        dp[0] = 0
 
+        # dp[n]에 전체 책장을 채울 때의 최소 높이가 저장
+        for i in range(1, n + 1):
+            width = 0
+            height = 0
+            for j in range(i - 1, -1, -1):
+                width += books[j][0]
+                if width > shelfWidth:
+                    break
+                height = max(height, books[j][1])
+                dp[i] = min(dp[i], height + dp[j])
+
+        return dp[n]
