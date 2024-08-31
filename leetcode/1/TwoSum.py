c8770d82604eb9cc2058fa3f619bcfa081d8bd2d
letzgorats <a01030659322@gmail.com>
2024-08-31 12:56:46 +0900

    hash table,two pointers,sort,dict,nums,array

--- leetcode/1/TwoSum.py
+++ leetcode/1/TwoSum.py
@@ -1,3 +1,4 @@
+# first solution
 class Solution(object):
     def twoSum(self, nums, target):
 
@@ -23,4 +24,29 @@ class Solution(object):
 
                 break
         
+        return answer
+
+
+# second solution
+from collections import defaultdict
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        nums_dict = defaultdict(list)
+        for idx, num in enumerate(nums):
+            nums_dict[num].append(idx)
+
+        answer = []
+        for k, v in nums_dict.items():
+
+            if target - k in nums_dict.keys():
+
+                if target - k == k and len(nums_dict[k]) != 1:
+                    return nums_dict[k]
+                elif target - k != k:
+
+                    answer.append(nums_dict[k][0])
+                    answer.append(nums_dict[target - k][0])
+                    break
+
         return answer
