5a6389e19b8fda81fadb11ce2498f063546f9ecb
letzgorats <a01030659322@gmail.com>
2024-09-05 17:15:40 +0900

    math,simulation,sum,mod,greedy,division

--- leetcode/2028/Find Missing Observations.py
+++ leetcode/2028/Find Missing Observations.py
@@ -1,3 +1,41 @@
+# wrong answer
+class Solution:
+    def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
+
+        m = len(rolls)
+        total = m + n
+
+        m_val = sum(rolls)
+        n_val = (mean * total) - m_val
+
+        answer = []
+        if n <= n_val <= 6 * n:
+            avg = n_val // n
+            mod = n_val % n
+
+            if mod:
+                for _ in range(n - 1):
+                    answer.append(avg)
+
+                if mod >= 6:
+                    i = mod // 6
+                    j = mod % 6
+                    for k in range(i):
+                        answer[k] = 6
+                    answer.append(j)
+                else:
+                    answer.append(avg + mod)
+            else:
+                for _ in range(n):
+                    answer.append(avg)
+
+            return answer
+        else:
+            return []
+
+
+
+# solution
 class Solution:
     def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
 
