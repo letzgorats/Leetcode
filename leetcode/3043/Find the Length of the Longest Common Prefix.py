aac873a9f41c128178d1f86e4823a05c8f56680b
letzgorats <a01030659322@gmail.com>
2024-09-25 00:18:23 +0900

    hash,string,trie,array,bfs,dfs,binary serach tree,set

--- leetcode/3043/Find the Length of the Longest Common Prefix.py
+++ leetcode/3043/Find the Length of the Longest Common Prefix.py
@@ -15,11 +15,40 @@ class Solution:
                 else:
                     a = str(num2)
                     b = str(num1)
+
                 for i in range(len(b)):
+
                     if a[i] == b[i]:
                         tmp += 1
+                    else:
+                        break
                 answer = max(answer, tmp)
+
 
         return answer
 
+# solution
+class Solution:
+    def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
+
+        store = set()
+        arr1Set = set(arr1)
+        arr2Set = set(arr2)
+
+        for val in arr2Set:
+            cur = ""
+            for ch in str(val):
+                cur += ch
+                if int(cur) not in store:
+                    store.add(int(cur))
+
+        answer = 0
+        for val in arr1Set:
+            cur = ""
+            for ch in str(val):
+                cur += ch
+                if int(cur) in store:
+                    answer = max(answer, int(len(cur)))
+
+        return answer
 
