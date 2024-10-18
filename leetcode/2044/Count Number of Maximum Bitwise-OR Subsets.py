752681024a16cc0d87b7d2dc0a35ed4dce556a60
letzgorats <a01030659322@gmail.com>
2024-10-19 00:24:22 +0900

    dfs,backtracking,bit manipulation,enumeration,revursive,subset,or,bitwise,memoization,bottom up,bitmask,dp

--- leetcode/2044/Count Number of Maximum Bitwise-OR Subsets.py
+++ leetcode/2044/Count Number of Maximum Bitwise-OR Subsets.py
@@ -21,3 +21,68 @@ class Solution:
 
         dfs(0, 0)
         return res
+
+
+class Solution:
+    def countMaxOrSubsets(self, nums: List[int]) -> int:
+
+        max_or = 0
+        for n in nums:
+            max_or |= n
+
+        def dfs(i, cur_or):
+            nonlocal max_or
+            if i == len(nums):
+                return 1 if cur_or == max_or else 0
+
+            # skip i + include i
+            return dfs(i + 1, cur_or) + dfs(i + 1, cur_or | nums[i])
+
+        return dfs(0, 0)
+
+# solution 2 - memoization
+class Solution:
+    def countMaxOrSubsets(self, nums: List[int]) -> int:
+
+        max_or = 0
+        for n in nums:
+            max_or |= n
+
+        cache = [[-1] * (max_or + 1) for _ in range(len(nums))]
+
+        def dfs(i, cur_or):
+            nonlocal max_or
+            if i == len(nums):
+                return 1 if cur_or == max_or else 0
+
+            if cache[i][cur_or] != -1:
+                return cache[i][cur_or]
+
+            cache[i][cur_or] = (
+                    dfs(i + 1, cur_or) +
+                    dfs(i + 1, cur_or | nums[i])
+            )
+            return cache[i][cur_or]
+
+        return dfs(0, 0)
+
+# solution 3 - bottom up solution - dp
+class Solution:
+    def countMaxOrSubsets(self, nums: List[int]) -> int:
+
+        dp = defaultdict(int)  # (key = cur_or ,val=cnt)
+        dp[0] = 1
+        max_or = 0
+
+        for n in nums:
+            new_dp = deepcopy(dp)
+            for cur_or, cnt in dp.items():
+                new_or = n | cur_or
+                new_dp[new_or] += cnt
+
+            dp = new_dp
+            max_or |= n
+
+        return dp[max_or]
+
+
