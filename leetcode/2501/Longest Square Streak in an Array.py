225e6f3375495451941341c8d2d756a944e3d6b5
letzgorats <a01030659322@gmail.com>
2024-10-29 00:07:58 +0900

    hash,dict,defaultdict,count,pow,max_length,longest,squre,streak,number,sort

--- leetcode/2501/Longest Square Streak in an Array.py
+++ leetcode/2501/Longest Square Streak in an Array.py
@@ -1,2 +1,18 @@
 class Solution:
     def longestSquareStreak(self, nums: List[int]) -> int:
+
+        nums.sort()
+        num_set = set(nums)
+        max_length = 0
+        for num in nums:
+            length = 0
+            while num in num_set:
+                length += 1
+                num = num ** 2
+
+            max_length = max(max_length, length)
+
+            if max_length == 5:
+                return 5
+
+        return max_length if max_length >= 2 else -1
