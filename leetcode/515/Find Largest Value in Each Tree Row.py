cacd1307cb7d2b60f65cfeec36a4ebcc6d8f2b07
letzgorats <a01030659322@gmail.com>
2024-12-25 16:30:33 +0900

    queue, dfs, bfs, stack, tree, level, binary tree, max_val, defaultdict, traversal

--- leetcode/515/Find Largest Value in Each Tree Row.py
+++ leetcode/515/Find Largest Value in Each Tree Row.py
@@ -1,3 +1,5 @@
+# solution 1 - bfs
+from collections import deque
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
@@ -29,5 +31,37 @@ class Solution(object):
                 if temp.right:
                     queue.append(temp.right)
             answer.append(level_max)
+
+        return answer
+
+# solution 2 - dfs
+
+from collections import defaultdict
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+
+        level_per_val = defaultdict(list)
+
+        def dfs(node, level):
+
+            if not node:
+                return
+            level_per_val[level].append(node.val)
+
+            if node.left:
+                dfs(node.left, level + 1)
+            if node.right:
+                dfs(node.right, level + 1)
+
+        dfs(root, 1)
+        answer = []
+        for k, tmp in level_per_val.items():
+            answer.append(max(tmp))
 
         return answer
