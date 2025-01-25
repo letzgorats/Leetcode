de19208b39b5f631468756a9e3309f426dabee99
letzgorats <a01030659322@gmail.com>
2025-01-25 10:21:55 +0900

    union find,graph,node,index,DSU,swap,limit,lexicographically smallest,element,sorting,array,connect,deque

--- leetcode/2948/Make Lexicographically Smallest Array by Swapping Elements.py
+++ leetcode/2948/Make Lexicographically Smallest Array by Swapping Elements.py
@@ -26,4 +26,27 @@ class Solution:
 
         return answer
 
-# solution 2 - DSU
+# solution 2 - deque
+from collections import deque
+class Solution:
+    def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
+
+        groups = []  # list of queues
+
+        num_to_group = {}  # k : v -> nums[i] : groups index
+
+        for n in sorted(nums):
+
+            if not groups or abs(n - groups[-1][-1]) > limit:
+                groups.append(deque())
+
+            groups[-1].append(n)
+            num_to_group[n] = len(groups) - 1
+
+        res = []
+
+        for n in nums:
+            j = num_to_group[n]
+            res.append(groups[j].popleft())
+
+        return res
