3b08257d23ea6ea1214c22a2f5717e1009bdf2d9
letzgorats <a01030659322@gmail.com>
2025-01-27 22:46:21 +0900

    deque,indegree,toplogical sort,graph,matrix,grid,numCourse,prerequisites,isReachable,bfs,dfs,reach,node,next_course,schedule

---
+++ leetcode/1462/Course Schedule IV.py
@@ -0,0 +1,36 @@
+from collections import deque
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[
+        bool]:
+
+        graph = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+        for a, b in prerequisites:
+            graph[a].append(b)
+            indegree[b] += 1
+
+        INF = float('inf')
+        isReachable = [[False] * numCourses for _ in range(numCourses)]
+
+        q = deque()
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
+
+        while q:
+
+            now = q.popleft()
+            for next_course in graph[now]:
+
+                for i in range(numCourses):
+                    if isReachable[i][now]:
+                        isReachable[i][next_course] = True
+
+                # 자기 자신도 도달 가능성 추가
+                isReachable[now][next_course] = True
+
+                indegree[next_course] -= 1
+                if indegree[next_course] == 0:
+                    q.append(next_course)
+
+        return [isReachable[a][b] for a, b in queries]
