1138a8b7648901da25c19d5e2f05fa5a77039b4a
letzgorats <a01030659322@gmail.com>
2025-03-20 14:02:15 +0900

    component,union,find,parent,size,tree,graph,root,minimum cost,weighted graph,bitwise &,ampersand,and operator

--- leetcode/3108/Minimum Cost Walk in Weighted Graph.py
+++ leetcode/3108/Minimum Cost Walk in Weighted Graph.py
@@ -1,7 +1,12 @@
+# solution 1 - union find, graph, bitwise - (192ms) - (2025.03.20)
+'''
+bitwise AND 연산을 하면 항상 작은 쪽으로 값이 수렴하는 특성이 있다.
+때문에, 같은 컴포넌트 내의 값을 다 & 하는 것이 최소값을 찾는 것을 보장한다.
+'''
 class UnionFind:
     def __init__(self, n):
         self.parent = list(range(n))
-        self.size = [1] * n  # 각각의 노드의 사이즈(처음은 자기자신 1개)
+        self.size = [1] * n  # 모든 집합은 처음엔 자기 자신만 포함
 
     def find(self, x):
         if x != self.parent[x]:
@@ -55,7 +60,11 @@ class Solution:
             else:
                 res.append(component_cost[r1])
 
-        print(res)
-
         return res
+
+'''
+union_find 에서 union 함수를 짤 때, 한쪽의 컴포넌트의 깊이가 치우치지 않고 각 컴포넌트의 깊이가 고르게 하려면 (그래야, find 연산의 시간도 빨라진다.)
+parent 배열만 선언하는 것이 아니라 size 배열(component 크기)도 선언해서 작은 사이즈의 컴포넌트를 큰 쪽으로 흡수시키는 코드로 짜는게 효율적이다.
+union find 알고리즘의 효율적인 코드를 다시 또 배웠다.
+'''
 
