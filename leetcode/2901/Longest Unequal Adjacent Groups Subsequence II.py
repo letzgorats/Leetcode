2b4a8559834ecbe56a94a3c490606284b31de655
letzgorats <a01030659322@gmail.com>
2025-05-17 01:18:39 +0900

    longest unequal adjacent groups,subsequence,dp,top-down,dfs,hamming distance,take,skip,memoization,lru_cache

--- leetcode/2901/Longest Unequal Adjacent Groups Subsequence II.py
+++ leetcode/2901/Longest Unequal Adjacent Groups Subsequence II.py
@@ -1,5 +1,34 @@
-# solutino 1 - (dfs) - (2025.05.16)
+# solutino 1 - (dfs,memoization) - (4090ms) - (2025.05.16)
+from functools import lru_cache
+class Solution:
+    def getWordsInLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
 
+        def hamming_distance(a, b):
+            return sum(c1 != c2 for c1, c2 in zip(a, b))
+
+        n = len(words)
+
+        @lru_cache(None)
+        def dfs(i, last_group, last_word):
+            if i == n:
+                return []
+            take = []
+            if len(words[i]) == len(last_word) and hamming_distance(words[i], last_word) == 1:
+                if groups[i] != last_group:
+                    take = [words[i]] + dfs(i + 1, groups[i], words[i])
+            skip = dfs(i + 1, last_group, last_word)
+            return max(len(take),len(skip))
+
+        res = []
+        for i in range(n):
+            curr = [words[i]] + dfs(i + 1, groups[i], words[i])
+            if len(curr) > len(res):
+                res = curr
+
+        return res
+
+
+# solution 2 - (topological sort) - (2025.05.16)
 
 
 # wrong answer - 최적의 답 보장 못함(그리디)
