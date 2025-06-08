5be1741cbe34a267b5327447ad82e847a17797aa
letzgorats <a01030659322@gmail.com>
2025-06-08 15:02:13 +0900

    dfs,iterative,O(n),lexicographical number,sort,heapq,recursive

--- leetcode/386/Lexicographical Numbers.py
+++ leetcode/386/Lexicographical Numbers.py
@@ -1,4 +1,5 @@
-# solution 1 - recursive, O(n) , O(n)
+# solution 1 - recursive - O(n) - (2024.09.21)
+from typing import List
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
 
@@ -19,7 +20,7 @@ class Solution:
 
         return result
 
-# solution 2 - iterative, math, O(n), O(n)
+# solution 2 - iterative, math, O(n), O(n) - (2024.09.21)
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
 
@@ -33,12 +34,12 @@ class Solution:
             else:
                 while cur == n or cur % 10 == 9:
                     cur = cur // 10
+
                 cur += 1
 
         return result
 
-
-# solution 3 - O(nlogn), O(nlogn)
+# solution 3 - O(nlogn), O(nlogn) - (2024.09.21)
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
         nums = [str(i) for i in range(1, n + 1)]
@@ -47,3 +48,38 @@ class Solution:
 
         return nums
 
+# solution 4 - (dfs,recursive) - (48ms) - (2025.06.08)
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+
+        ans = []
+
+        def recurse(current):
+
+            if current > n:
+                return
+
+            ans.append(current)
+            for i in range(0, 10):
+                if current * 10 + i <= n:
+                    recurse(current * 10 + i)
+
+        for i in range(1, 10):
+            if i <= n:
+                recurse(i)
+
+        return ans
+
+# solution 5 - heapq, O(n logn) - (2025.06.08)
+import heapq
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        q = []
+        for i in range(1, n + 1):
+            heapq.heappush(q, str(i))
+        # print(q)
+        ans = []
+        while q:
+            ans.append(int(heapq.heappop(q)))
+
+        return ans
