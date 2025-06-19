bcea1acd5e89b0aece3fb5e9b6f11673c954b0b5
letzgorats <a01030659322@gmail.com>
2025-06-20 00:06:22 +0900

    greedy,sorting,min,max,sequence,split,difference,partition

--- leetcode/2294/Partition Array Such That Maximum Difference Is K.py
+++ leetcode/2294/Partition Array Such That Maximum Difference Is K.py
@@ -1,1 +1,18 @@
-# solution 1-
+# solution 1 - (greedy,sorting) - (133ms) - (2025.06.20)
+from typing import List
+class Solution:
+    def partitionArray(self, nums: List[int], k: int) -> int:
+
+        nums.sort()
+        # print(nums)
+        min_val = nums[0]
+        ans = 1
+        for i in range(1, len(nums)):
+
+            if nums[i] - min_val <= k:
+                min_val = min(nums[i], min_val)
+            else:
+                ans += 1
+                min_val = nums[i]
+
+        return ans
