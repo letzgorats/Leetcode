2ee731f975f9f1297c0d1776e9a4894fa725d8f8
letzgorats <a01030659322@gmail.com>
2025-07-09 00:12:17 +0900

    lru_cache,dp,bisect,binary search,events,maximum number of events that can be attended,sorting,dynamic programming

--- leetcode/1751/Maximum Number of Events That Can Be Attended II.py
+++ leetcode/1751/Maximum Number of Events That Can Be Attended II.py
@@ -1,1 +1,23 @@
-# solution 1 - (dp) - () - (2025.07.08)
+# solution 1 - (dp+binary search) - (1050ms) - (2025.07.08)
+from functools import lru_cache
+import bisect
+from typing import List
+class Solution:
+    def maxValue(self, events: List[List[int]], k: int) -> int:
+        events.sort()
+        starts = [e[0] for e in events]
+
+        @lru_cache(None)
+        def dp(i, remaining):
+            if i == len(events) or remaining == 0:
+                return 0
+
+            # skip
+            skip = dp(i + 1, remaining)
+            # take
+            j = bisect.bisect_right(starts, events[i][1])  # 다음 가능한 이벤트 인덱스
+            take = events[i][2] + dp(j, remaining - 1)
+
+            return max(skip, take)
+
+        return dp(0, k)
