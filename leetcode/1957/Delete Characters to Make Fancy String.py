4f4b8e11327d399740948b65b670abb0ba1c653c
letzgorats <a01030659322@gmail.com>
2025-07-21 13:52:51 +0900

    count,string,alphabet,consecutive,continuous,dict,frequency,alphabet

--- leetcode/1957/Delete Characters to Make Fancy String.py
+++ leetcode/1957/Delete Characters to Make Fancy String.py
@@ -1,3 +1,4 @@
+# solution 1 - (string, count) - (279ms) - (2024.11.01)
 class Solution:
     def makeFancyString(self, s: str) -> str:
 
@@ -13,3 +14,45 @@ class Solution:
                 cnt = 0
 
         return ''.join(s)
+
+# solution 2 - (string, dict) - (541ms) - (2025.07.21)
+from collections import defaultdict
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+
+        cons_freq = defaultdict(int)
+        prev, answer = "", ""
+        for char in s:
+            if prev == char:
+                cons_freq[char] += 1
+                if cons_freq[char] >= 3:
+                    continue
+            else:
+                if cons_freq[prev]:
+                    del cons_freq[prev]
+                prev = char
+                cons_freq[char] += 1
+            answer += char
+
+        return answer
+
+# solution 3 - (count,string) - (191ms) - (2025.07.21)
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+
+        cnt = 0
+        prev, answer = "", ""
+        for char in s:
+            if prev == char:
+                cnt += 1
+                if cnt >= 2:
+                    continue
+            else:
+                cnt = 0
+                prev = char
+            answer += char
+
+        return answer
+
+
+
