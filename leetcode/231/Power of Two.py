c0cc3c3ee9065c685a5310d0b0a51336b38db06d
letzgorats <a01030659322@gmail.com>
2025-08-09 15:07:08 +0900

    math,bit manipulation,recursion,iterative,follow up,without loops,recursion,power oftwo,ampersand,and operation

--- leetcode/231/Power of Two.py
+++ leetcode/231/Power of Two.py
@@ -1,5 +1,5 @@
 # iterative solution
-# Time : O(nlogn)  / Space : O(1)
+# Time : O(nlogn)  / Space : O(1) - (2024.02.19)
 class Solution(object):
     def isPowerOfTwo(self, n):
         """
@@ -14,7 +14,7 @@ class Solution(object):
 
 
 # Follow up: without loops/recursion - Pattern in Power of 2
-# Time : O(nlogn)  / Space : O(1)
+# Time : O(nlogn)  / Space : O(1) - (2024.02.19)
 class Solution(object):
     def isPowerOfTwo(self, n):
         """
@@ -25,7 +25,7 @@ class Solution(object):
         return n > 0 and bin(n).count('1') == 1
 
 # Math solution
-# Time : O(1)  / Space : O(1)
+# Time : O(1)  / Space : O(1) - (2024.02.19)
 class Solution(object):
     def isPowerOfTwo(self, n):
         """
@@ -37,7 +37,7 @@ class Solution(object):
 
 
 # Pre-Compute all powers of 2 solution
-# Time : O(1)  / Space : O(1)
+# Time : O(1)  / Space : O(1) - (2024.02.19)
 class Solution(object):
     def isPowerOfTwo(self, n):
         """
@@ -46,3 +46,16 @@ class Solution(object):
         """
         power_of_2 = set(2**i for i in range(31))
         return n in power_of_2
+
+
+# solution 5 - (bit manipulation) - (0ms) - (2025.08.09)
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n > 0 and n & (n - 1) == 0:
+            return True
+
+        return False
+'''
+n 이 2의 승수라면, 항상 이진수는 1개인데, 
+n-1 과 and 연산을 하게 되면, 0 이 된다는 성질을 이용
+'''
