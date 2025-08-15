5726f7949584ce069a7da20932d147ef454f9e28
letzgorats <a01030659322@gmail.com>
2025-08-15 18:21:53 +0900

    math,log(n,base),count,bitwise,bit manipulation,(n >> i) & 1,bit_length(),power of Four

--- leetcode/342/Power of Four.py
+++ leetcode/342/Power of Four.py
@@ -1,3 +1,5 @@
+# solution 1 - (math) - (14ms) - (2023.10.23)
+import math
 class Solution(object):
     def isPowerOfFour(self, n):
         """
@@ -5,3 +7,19 @@ class Solution(object):
         :rtype: bool
         """
         return n > 0 and math.log(n,4) == int(math.log(n,4))
+
+# solution 2 - (bitwise) - (0ms) - (2025.08.15)
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+
+        p = str(bin(n)[2:])
+
+        if n < 0 or p.count('1') != 1:
+            return False
+
+        for i in range(0, n.bit_length(), 2):
+
+            if (n >> i) & 1:
+                return True
+
+        return False
