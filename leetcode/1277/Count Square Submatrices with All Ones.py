ccba598a859851cb3de9db3fc768cbb25b5e8f8a
letzgorats <a01030659322@gmail.com>
2025-08-20 15:12:35 +0900

    cache,matrix,submatrices with all ones,square,grid,dfs,helper,memoization,dynamic programming,dp,hash table,count,bottom-up,top-down

--- leetcode/1277/Count Square Submatrices with All Ones.py
+++ leetcode/1277/Count Square Submatrices with All Ones.py
@@ -1,4 +1,5 @@
 # solution 1 - dfs, cache - 206ms, 37.4mb
+from typing import List
 class Solution:
     def countSquares(self, matrix: List[List[int]]) -> int:
 
@@ -90,3 +91,73 @@ class Solution:
             dp = cur_dp
 
         return cnt
+
+
+# solution 5 - (dp,dfs) - (276ms) - (2025.08.20)
+'''
+기준이 현재 cell 이 top-left 일 때
+'''
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+
+        # maximal square 문제 먼저 풀고 이 문제 풀면 이해 잘됨.
+        # https://youtu.be/6X7Ha2PrDmM?si=VLkUZbvXfJRGi5-X
+        # -> 이해 완료
+
+        rows = len(matrix)
+        cols = len(matrix[0])
+        cache = defaultdict(int)
+
+        # 기준 : 현재 셀이 top-left-cell 일 때
+        def helper(r, c):
+
+            if r >= rows or c >= cols or matrix[r][c] == 0:
+                return 0
+
+            if (r, c) not in cache:
+
+                cache[(r, c)] = 0
+                if matrix[r][c] == 1:
+                    cache[(r, c)] = 1 + min(
+                        helper(r + 1, c),
+                        helper(r + 1, c + 1),
+                        helper(r, c + 1)
+                    )
+            return cache[(r, c)]
+
+        answer = 0
+        for r in range(rows):
+            for c in range(cols):
+                answer += helper(r, c)
+
+        print(cache)
+
+        return answer
+
+# solution 5 - (dp,dfs) - (66ms) - (2025.08.20)
+'''
+기준이 현재 cell 이 bottom-right 일 때
+'''
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+
+        # maximal square 문제 먼저 풀고 이 문제 풀면 이해 잘됨.
+        # https://youtu.be/6X7Ha2PrDmM?si=VLkUZbvXfJRGi5-X
+        # -> 이해 완료
+
+        # 기준 : 현재 셀이 down-bottom-cell 일 때
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        dp = [[0] * (cols + 1) for _ in range(rows + 1)]
+        # print(dp)
+
+        answer = 0
+        for i in range(rows):
+            for j in range(cols):
+                if matrix[i][j] > 0:
+                    dp[i + 1][j + 1] = 1 + min(dp[i + 1][j], dp[i][j], dp[i][j + 1])
+                    answer += dp[i + 1][j + 1]
+
+        print(dp)
+        return answer
