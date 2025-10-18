1df2cc0f912cb9bd75509bb0af01000859173c39
letzgorats <a01030659322@gmail.com>
2025-10-18 23:38:52 +0900

    sorting,greedy,nums,maximum number,distince,operations,elements,max,min,optimization

---
+++ leetcode/3397/Maximum Number of Distinct Elements After Operations.py
@@ -0,0 +1,18 @@
+# solution 1 - (sorting,greedy) - (721ms) - (2025.10.18)
+from typing import List
+class Solution:
+    def maxDistinctElements(self, nums: List[int], k: int) -> int:
+
+        nums.sort()
+        res = 0
+        smallest_elem = nums[0] - k - 1
+
+        for num in nums:
+
+            if num + k > smallest_elem:
+                res += 1
+                smallest_elem = max(smallest_elem + 1, num - k)
+
+            # print(smallest_elem)
+
+        return res
