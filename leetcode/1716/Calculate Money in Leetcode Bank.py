6072585bd5b5175bb4d904c6f354c2e464baea9b
letzgorats <a01030659322@gmail.com>
2025-10-25 13:25:41 +0900

    math,modular,greedy,simulation,divmod,divide,day,money,deposit,accumulative,bank

--- leetcode/1716/Calculate Money in Leetcode Bank.py
+++ leetcode/1716/Calculate Money in Leetcode Bank.py
@@ -1,4 +1,27 @@
-# sol 1)
+# solution 3 - (simulation,greedy) - (0ms) - (2025.10.25)
+class Solution:
+    def totalMoney(self, n: int) -> int:
+
+        window = [1, 2, 3, 4, 5, 6, 7]
+
+        if n // 7 == 0:
+            return sum(window[:n % 7])
+        else:
+            ans = 0
+            week = n // 7
+            ans += (28 * week)  # 한주 계산
+            # 주간 누적값 계산(월요일마다 증가
+            for i in range(1, week):
+                ans += 7 * i
+            # 마지막 주간 누적값 + 계산
+            ans += (n % 7) * week
+            ans += sum(window[:n % 7])
+
+        return ans
+
+
+
+# sol 1) - 21ms - (2023.12.06)
 class Solution(object):
     def totalMoney(self, n):
 
@@ -22,7 +45,7 @@ class Solution(object):
         
         return answer
 
-  # sol2)
+  # sol2) - 21ms - (2023.12.06)
   class Solution(object):
     def totalMoney(self, n):
 
