fde16bbee98a6f3033cc7571ecf73bf36c5aadb3
letzgorats <a01030659322@gmail.com>
2025-11-01 13:46:21 +0900

    ListNode,implementation,design,node,root,next,val,hash table,Linked List,head,array

--- leetcode/3217/Delete Nodes From Linked List Present in Array.py
+++ leetcode/3217/Delete Nodes From Linked List Present in Array.py
@@ -1,9 +1,45 @@
-# solution 1
+# solution 3 - (ListNode,set) - (50ms) - (2025.11.01)
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+from typing import List, Optional
+class Solution:
+    def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
+
+        values_to_remove = set(nums)
+
+        while head and head.val in values_to_remove:
+            head = head.next
+
+        if not head:
+            return None
+
+        current = head
+        # print(head)
+        while current.next:
+
+            if current.next.val in values_to_remove:
+                # skip
+                current.next = current.next.next
+            else:
+                current = current.next
+
+            # print(current)
+
+        return head
+
+
+# solution 1 - (design,ListNode) - (715ms) - (2024.09.06)
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+from typing import List
 class Solution:
     def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
 
@@ -24,7 +60,7 @@ class Solution:
 
         return dummy.next
 
-# solution 2
+# solution 2 - (design,ListNode) - (745ms) - (2024.09.06)
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
