ce8b193184125d444f92d44cd72333b744689094
letzgorats <a01030659322@gmail.com>
2025-11-21 11:36:49 +0900

    find,rfind,defaultdict,hash table,counting,set,len,index,palindromic,subsequence,unique,length-3

--- leetcode/1930/Unique Length-3 Palindromic Subsequences.py
+++ leetcode/1930/Unique Length-3 Palindromic Subsequences.py
@@ -1,3 +1,41 @@
+# solution 4 - (find,rfind) - (99ms) - (2025.11.21)
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        substring = defaultdict(list)
+        ans = 0
+        uniq_s = set(s)
+        for ch in uniq_s:
+            st = s.find(ch)
+            ed = s.rfind(ch)
+            ans += len(set(s[st + 1:ed]))
+
+        return ans
+
+# solution 3 - (defaultdict,substring) - (139ms) - (2025.11.21)
+from collections import defaultdict
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+
+        substring = defaultdict(list)
+
+        for i, ch in enumerate(s):
+            substring[ch].append(i)
+
+        ans = 0
+        for ch in substring:
+
+            if len(substring[ch]) < 2:
+                continue
+
+            st = substring[ch][0]
+            ed = substring[ch][-1]
+
+            ans += len(set(s[st + 1:ed]))
+
+        return ans
+
+
+# solution 2 - (defaultdict,substring) - (170ms) - (2025.01.04)
 class Solution(object):
     def countPalindromicSubsequence(self, s):
         """
@@ -25,6 +63,7 @@ class Solution(object):
         return cnt
 
 
+# solution 1 - (count,find) - (228ms) - (2023.11.14)
 class Solution(object):
     def countPalindromicSubsequence(self, s):
         """
