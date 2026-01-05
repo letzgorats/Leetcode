2a248271d96e01c2910981afbfa3ce79f750f591
letzgorats <a01030659322@gmail.com>
2026-01-05 10:17:00 +0900

    matrix,greedy,array,operations,even,odd,math,abs,min,max,rule,negative,positive,grid

--- leetcode/1975/Maximum Matrix Sum.py
+++ leetcode/1975/Maximum Matrix Sum.py
@@ -1,3 +1,25 @@
+# solution 2 - (greedy,matrix) - (115ms) - (2026.01.05)
+from typing import List
+class Solution:
+    def maxMatrixSum(self, matrix: List[List[int]]) -> int:
+
+        n = len(matrix)
+        absolute_val = 0
+        cnt = 0
+        smallest_val = float('inf')
+        for i in range(n):
+            for j in range(n):
+                if matrix[i][j] < 0:  # negative
+                    cnt += 1
+                absolute_val += abs(matrix[i][j])
+                smallest_val = min(smallest_val, abs(matrix[i][j]))
+        if cnt % 2 == 0:  # even-negative
+            return absolute_val
+        else:  # odd-negative
+            return absolute_val - 2 * smallest_val
+
+# solution 1 - (greedy,.matrix) - (117ms) - (2024.11.24)
+
 class Solution:
     def maxMatrixSum(self, matrix: List[List[int]]) -> int:
 
